24b220d5a4bde49d73af0a8ad8943d52d59b4a80
Justin Lee <jel@oath.com>
2017-12-23 16:09:48 -0600

    Added files for third cost lab

--- src/cost.h
+++ src/cost.h
@@ -1,28 +1,17 @@
 #ifndef COST_H
 #define COST_H
-#include <vector>
+#include "vehicle.h"
 
-/**
- * Cost becomes higher for trajectories with intended lane and final lane that have traffic slower than target_speed.
- *
- * @param target_speed
- * @param intended_lane
- * @param final_lane
- * @param lane_speeds
- * @return
- */
-float inefficiency_cost(int target_speed, int intended_lane, int final_lane, std::vector<int> lane_speeds);
+using namespace std;
 
-/**
- * The cost increases with both the distance of intended lane from the goal and the distance of the final lane from the goal.
- * The cost of being out of the goal lane also becomes larger as vehicle approaches the goal.
- *
- * @param goal_lane
- * @param intended_lane
- * @param final_lane
- * @param distance_to_goal
- * @return
- */
-float goal_distance_cost(int goal_lane, int intended_lane, int final_lane, float distance_to_goal);
+float calculate_cost(const Vehicle &vehicle, const map<int, vector<Vehicle>> &predictions, const vector<Vehicle> &trajectory);
+
+float goal_distance_cost(const Vehicle &vehicle, const vector<Vehicle> &trajectory, const map<int, vector<Vehicle>> &predictions, map<string, float> &data);
+
+float inefficiency_cost(const Vehicle &vehicle, const vector<Vehicle> &trajectory, const map<int, vector<Vehicle>> &predictions, map<string, float> &data);
+
+float lane_speed(const map<int, vector<Vehicle>> &predictions, int lane);
+
+map<string, float> get_helper_data(const Vehicle &vehicle, const vector<Vehicle> &trajectory, const map<int, vector<Vehicle>> &predictions);
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,59 +1,64 @@
-#include <iostream>
-#include "cost.h"
+#include "road.h"
 
 using namespace std;
 
+// Impacts default behavior for most states
+int SPEED_LIMIT = 10;
+
+// All traffic in lane (besides ego) follow these speeds
+vector<int> LANE_SPEEDS = {6, 7, 8, 9};
+
+// Number of available "cells" which should have traffic
+double TRAFFIC_DENSITY = 0.15;
+
+// At each timestep, ego can set acceleration to value between
+// -MAX_ACCEL and MAX_ACCEL
+int MAX_ACCEL = 2;
+
+// s value and lane number of goal.
+vector<int> GOAL = {300, 0};
+
+// These affect the visualization
+int FRAMES_PER_SECOND = 4;
+int AMOUNT_OF_ROAD_VISIBLE = 40;
+
 int main() {
 
-  //Target speed of our vehicle
-  int target_speed = 10;
+  Road road = Road(SPEED_LIMIT, TRAFFIC_DENSITY, LANE_SPEEDS);
 
-  //Lane speeds for each lane
-  vector<int> lane_speeds = {6, 7, 8, 9};
+  road.update_width = AMOUNT_OF_ROAD_VISIBLE;
 
-  //Test cases used for grading - do not change.
-  float cost;
-  cout << "Costs for (intended_lane, final_lane):" << endl;
-  cout << "----------------------------------------------------------" << endl;
-  cost = inefficiency_cost(target_speed, 3, 3, lane_speeds);
-  cout << "The cost is " << cost << " for " << "(2, 2)" << endl;
-  cost = inefficiency_cost(target_speed, 2, 3, lane_speeds);
-  cout << "The cost is " << cost << " for " << "(2, 2)" << endl;
-  cost = inefficiency_cost(target_speed, 2, 2, lane_speeds);
-  cout << "The cost is " << cost << " for " << "(2, 2)" << endl;
-  cost = inefficiency_cost(target_speed, 1, 2, lane_speeds);
-  cout << "The cost is " << cost << " for " << "(1, 2)" << endl;
-  cost = inefficiency_cost(target_speed, 1, 1, lane_speeds);
-  cout << "The cost is " << cost << " for " << "(1, 1)" << endl;
-  cost = inefficiency_cost(target_speed, 0, 1, lane_speeds);
-  cout << "The cost is " << cost << " for " << "(0, 1)" << endl;
-  cost = inefficiency_cost(target_speed, 0, 0, lane_speeds);
-  cout << "The cost is " << cost << " for " << "(0, 0)" << endl;
+  road.populate_traffic();
 
-  return 0;
+  int goal_s = GOAL[0];
+  int goal_lane = GOAL[1];
 
-  /**
-  int goal_lane = 0;
+  // Configuration data: speed limit, num_lanes, goal_s, goal_lane, max_acceleration
 
-  //Test cases used for grading - do not change.
-  float cost;
-  cout << "Costs for (intended_lane, final_lane, goal_distance):" << endl;
-  cout << "----------------------------------------------------------" << endl;
-  cost = goal_distance_cost(goal_lane, 2, 2, 1.0);
-  cout << "The cost is " << cost << " for " << "(2, 2, 1.0)" << endl;
-  cost = goal_distance_cost(goal_lane, 2, 2, 10.0);
-  cout << "The cost is " << cost << " for " << "(2, 2, 10.0)" << endl;
-  cost = goal_distance_cost(goal_lane, 2, 2, 100.0);
-  cout << "The cost is " << cost << " for " << "(2, 2, 100.0)" << endl;
-  cost = goal_distance_cost(goal_lane, 1, 2, 100.0);
-  cout << "The cost is " << cost << " for " << "(1, 2, 100.0)" << endl;
-  cost = goal_distance_cost(goal_lane, 1, 1, 100.0);
-  cout << "The cost is " << cost << " for " << "(1, 1, 100.0)" << endl;
-  cost = goal_distance_cost(goal_lane, 0, 1, 100.0);
-  cout << "The cost is " << cost << " for " << "(0, 1, 100.0)" << endl;
-  cost = goal_distance_cost(goal_lane, 0, 0, 100.0);
-  cout << "The cost is " << cost << " for " << "(0, 0, 100.0)" << endl;
+  int num_lanes = LANE_SPEEDS.size();
+  vector<int> ego_config = {SPEED_LIMIT, num_lanes, goal_s, goal_lane, MAX_ACCEL};
+
+  road.add_ego(2, 0, ego_config);
+  int timestep = 0;
+
+  while (road.get_ego().s <= GOAL[0]) {
+    timestep++;
+    if (timestep > 100) {
+      break;
+    }
+    road.advance();
+    road.display(timestep);
+    //time.sleep(float(1.0) / FRAMES_PER_SECOND);
+  }
+  Vehicle ego = road.get_ego();
+  if (ego.lane == GOAL[1]) {
+    cout << "You got to the goal in " << timestep << " seconds!" << endl;
+    if (timestep > 35) {
+      cout << "But it took too long to reach the goal. Go faster!" << endl;
+    }
+  } else {
+    cout << "You missed the goal. You are in lane " << ego.lane << " instead of " << GOAL[1] << "." << endl;
+  }
 
   return 0;
-  */
 }
