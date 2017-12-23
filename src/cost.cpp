67dbd534e0dffc9a29f567f3b27232cfb8e6a464
Justin Lee <jel@oath.com>
2017-12-23 16:01:02 -0600

    Updated for second cost function

--- src/cost.cpp
+++ src/cost.cpp
@@ -4,6 +4,13 @@
 
 using namespace std;
 
+float inefficiency_cost(int target_speed, int intended_lane, int final_lane, vector<int> lane_speeds) {
+  float speed_intended = lane_speeds[intended_lane];
+  float speed_final = lane_speeds[final_lane];
+  float cost = (float) ((2.0 * target_speed - speed_intended - speed_final) / target_speed);
+  return cost;
+}
+
 float goal_distance_cost(int goal_lane, int intended_lane, int final_lane, float distance_to_goal) {
   int delta_d = (int) (2.0 * goal_lane - intended_lane - final_lane);
   float cost = 1 - exp(-(abs(delta_d) / distance_to_goal));
--- src/cost.h
+++ src/cost.h
@@ -1,5 +1,17 @@
 #ifndef COST_H
 #define COST_H
+#include <vector>
+
+/**
+ * Cost becomes higher for trajectories with intended lane and final lane that have traffic slower than target_speed.
+ *
+ * @param target_speed
+ * @param intended_lane
+ * @param final_lane
+ * @param lane_speeds
+ * @return
+ */
+float inefficiency_cost(int target_speed, int intended_lane, int final_lane, std::vector<int> lane_speeds);
 
 /**
  * The cost increases with both the distance of intended lane from the goal and the distance of the final lane from the goal.
